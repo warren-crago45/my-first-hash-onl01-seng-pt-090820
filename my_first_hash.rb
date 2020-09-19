985441be1891a0cdff5de77cb304bc5b6dba425f
Warren Crago <warren.crago45@gmail.com>
2020-09-19 02:18:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  your_hash = [""]
+  your_hash = ["apple" => "delicious fruit", "soda" => "carbonated beverage", "cheese" => "edible dairy"]
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
